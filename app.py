4be0281d2c5dec1ce2c0ba9db7f076ac52a6aee7
agna2103 <161094297+agna2103@users.noreply.github.com>
2025-11-01 08:20:36 +0700

    Introduce Air Quality Index section in app.py
    Added information about Air Quality Index and its significance.

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import seaborn as sns
 sns.set(style='dark')
 st.set_page_config(page_title= "AI Dalam Psikologi",page_icon="𝚿",layout= "wide")
 st.header("AI Dalam Psikologi 𝚿")
-
+with st.container():
+    st.subheader("Air Quality Index 🌍")
+    st.title("Apa itu Air Quality Index ?")
+    st.write("air quality index merupakan parameter yang dapat digunakan untuk menilai seberapa buruk tingkat polusi udara. air quality index menggunakan skala 1-500, semakin tinggi nilai aqi maka semakin buruk pollusi udara yang terjadi.")
+    st.write("[learn more >](https://www.airnow.gov/aqi/aqi-basics/)")
 with st.container():
     st.write("---")
     st.caption("agna aldhaka-contoh penggunaan cnn untuk deteksi gambar")
