a49ccf06d88271eeb3ff0141f6668117e59a6303
agna2103 <161094297+agna2103@users.noreply.github.com>
2025-11-01 08:17:35 +0700

    Change seaborn style from 'purple' to 'black'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import seaborn as sns
-sns.set(style='purple')
+sns.set(style='black')
 st.set_page_config(page_title= "AI Dalam Psikologi",page_icon="𝚿",layout= "wide")
 st.header("AI Dalam Psikologi 𝚿")
 
